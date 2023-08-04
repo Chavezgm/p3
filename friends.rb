f59af8f7df7ba4e734200a5bb360bcb57fe7e48a
Martin Chavez <martinchavez1203@gmail.com>
2023-08-04 17:31:41 -0600

    Add p2

---
+++ friends.rb
@@ -0,0 +1,12 @@
+friends = [10,20,30,40,50,60,70,80]
+friends.each do |friend|
+    if friend >= 30
+        puts friend
+    end 
+end 
+friends.each do |friend|
+    if friend <= 60
+        puts friend
+    end 
+end 
+
